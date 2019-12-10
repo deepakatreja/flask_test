53c75aaa24543347c750791ef9fff0fc81b0f117
deepakatreja <datreja@gmail.com>
2019-12-10 16:46:16 +0530

    seond commit

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask, jsonify
-myapp = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return '''
+Main <a href="/add/23/19">add</a>
+'''
+
+@app.route("/add/<int:a>/<int:b>")
+def api_info(a, b):
+    return str(a+b)
+
+if __name__ == '__main__': 
+  
+    app.run(debug=True, host='0.0.0.0') 
+    
 
 
-@myapp.route("/")
-def hello():
-    return "hello world"
+# from flask import Flask, jsonify
+# myapp = Flask(__name__)
+
+
+# @myapp.route("/")
+# def hello():
+#     return "hello world"
+
+
+# # @myapp.route("/deepak")
+# # def deepak():
+# #     return "hello deepak"
+
+# # @myapp.route("/manish")
+# # def manish():
+# #     return "hello manish"
+
+# # @myapp.route("/sahil")
+# # def sahils():
+# #     return "hello sahil"
+
+# # if __name__ == '__main__': 
+
+# # app.run(debug=True, host='0.0.0.0') 
+
--- test_app.py
+++ test_app.py
@@ -1,8 +1,55 @@
-from app import myapp
+from p1 import myapp
+import unittest
+
+# python -m unittest test
 
 
-def test_hello():
-    response = myapp.test_client().get('/')
+class TestMyApp(unittest.TestCase):
+    def setUp(self):
+        self.app = myapp.test_client()
 
-    assert response.status_code == 200
-    assert response.data == b'hello world'
+    def test_main(self):
+        rv = self.app.get('/')
+        assert rv.status == '200 OK'
+        #assert False
+
+    def test_add(self):
+        rv = self.app.get('/add/2/3')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, '5')
+
+        rv = self.app.get('/add/0/10')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, '10')
+
+    def test_404(self):
+        rv = self.app.get('/other')
+        self.assertEqual(rv.status, '404 NOT FOUND')
+
+# from app import myapp
+
+
+# def test_hello():
+#     response = myapp.test_client().get('/')
+
+#     assert response.status_code == 200
+#     assert response.data == b'hello world'
+
+# def test_deepak():
+#     response = myapp.test_client().get('/deepak')
+
+#     assert response.status_code == 200
+#     assert response.data == b'hello deepak'
+
+# def test_manish():
+#     response = myapp.test_client().get('/manish')
+
+#     assert response.status_code == 200
+#     assert response.data == b'hello manish'
+
+# def test_sahil():
+#     response = myapp.test_client().get('/sahil')
+
+#     assert response.status_code == 200
+#     assert response.data == b'hello sahil'
+
